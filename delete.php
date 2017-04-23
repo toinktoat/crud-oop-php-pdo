a1b65a400b5257270b994c2b67072ef5f659df0a
toinktoat <toat.hidayatulloh@gmail.com>
2017-04-23 20:15:34 +0700

    versi 1.0

---
+++ delete.php
@@ -0,0 +1,100 @@
+<?php
+require_once("class.crud.php");
+$crud = new CRUD();
+
+if(isset($_POST['btn-del']))
+{
+	$id = $_GET['delete_id'];
+	$crud->delete($id);
+	header("Location: delete.php?deleted");
+}
+
+?>
+
+<?php include_once 'header.php'; ?>
+
+<div class="clearfix"></div>
+
+<div class="container">
+
+	<?php
+	if(isset($_GET['deleted']))
+	{
+		?>
+        <div class="alert alert-success">
+    	<strong>Berhasil</strong> data telah terhapus ...
+		</div>
+        <?php
+	}
+	else
+	{
+		?>
+        <div class="alert alert-danger">
+    	<strong>Yakin ?</strong> untuk menghapus data ini ?
+		</div>
+        <?php
+	}
+	?>
+</div>
+
+<div class="clearfix"></div>
+
+<div class="container">
+
+	 <?php
+	 if(isset($_GET['delete_id']))
+	 {
+		 ?>
+         <table class='table table-bordered'>
+         <tr>
+         <th>No</th>
+         <th>First Name</th>
+         <th>Last Name</th>
+         <th>E - mail ID</th>
+         <th>Contact No</th>
+         </tr>
+         <?php
+         $stmt = $crud->runQuery("SELECT * FROM tbl_users WHERE id=:id");
+         $stmt->execute(array(":id"=>$_GET['delete_id']));
+         while($row=$stmt->fetch(PDO::FETCH_BOTH))
+         {
+             ?>
+             <tr>
+             <td><?php print($row['id']); ?></td>
+             <td><?php print($row['first_name']); ?></td>
+             <td><?php print($row['last_name']); ?></td>
+             <td><?php print($row['email_id']); ?></td>
+         	 <td><?php print($row['contact_no']); ?></td>
+             </tr>
+             <?php
+         }
+         ?>
+         </table>
+         <?php
+	 }
+	 ?>
+</div>
+
+<div class="container">
+<p>
+<?php
+if(isset($_GET['delete_id']))
+{
+	?>
+  	<form method="post">
+    <input type="hidden" name="id" value="<?php echo $row['id']; ?>" />
+    <button class="btn btn-large btn-primary" type="submit" name="btn-del"><i class="glyphicon glyphicon-trash"></i> &nbsp; YA</button>
+    <a href="index.php" class="btn btn-large btn-success"><i class="glyphicon glyphicon-backward"></i> &nbsp; TIDAK</a>
+    </form>
+	<?php
+}
+else
+{
+	?>
+    <a href="index.php" class="btn btn-large btn-success"><i class="glyphicon glyphicon-backward"></i> &nbsp; Kembali Ke Index</a>
+    <?php
+}
+?>
+</p>
+</div>
+<?php include_once 'footer.php'; ?>
---
+++ footer.php
@@ -0,0 +1,24 @@
+<div class="container">
+	
+</div>
+<div id="preloader">
+    <div id="preloader-inner"></div>
+</div>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="jquery.js"></script>
+<script src="parsleyjs/dist/parsley.min.js"></script>
+<script src="parsleyjs/dist/i18n/id.js"></script>
+<script src="preloader/dist/jquery.preloader.min.js" type="text/javascript"></script>	
+<script>
+$(document).ready(function(){
+	$('form').parsley();
+});
+</script><!-- Java script include -->
+<script>
+    $(window).preloader({
+        delay: 500
+    });
+</script>
+
+</body>
+</html>
